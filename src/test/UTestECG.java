e851d9bd466b34b631910998bf110f44387e74ac
audreyhsiao <audreyyj13@gmail.com>
2025-04-25 17:25:27 -0500

    update test case

--- src/test/UTestECG.java
+++ src/test/UTestECG.java
@@ -29,75 +29,75 @@ public class UTestECG {
 		assert(300.0 <= threshold && threshold <= 1500.0);
 	}
 
-	// @Test
-	// public void testHeartRate() {
-	// 	System.out.println("***** Test Average Heart Rate *****");
+	@Test
+	public void testHeartRate() {
+		System.out.println("***** Test Average Heart Rate *****");
 	
-	// 	Query<Integer,Double> q = HeartRate.qHeartRateAvg();
-	// 	SLastCount<Double> sink = S.lastCount();
+		Query<Integer,Double> q = HeartRate.qHeartRateAvg();
+		SLastCount<Double> sink = S.lastCount();
 		
-	// 	Q.execute(Data.ecgStream("100.csv"), q, sink);
+		Q.execute(Data.ecgStream("100.csv"), q, sink);
 
-	// 	assertEquals(1, sink.count);
-	// 	double threshold = sink.last;
-	// 	assert(50.0 <= threshold && threshold <= 100.0);
-	// }
+		assertEquals(1, sink.count);
+		double threshold = sink.last;
+		assert(50.0 <= threshold && threshold <= 100.0);
+	}
 
-	// @Test
-	// public void testSDNN() {
-	// 	System.out.println("***** Test SDNN *****");
+	@Test
+	public void testSDNN() {
+		System.out.println("***** Test SDNN *****");
 	
-	// 	Query<Integer,Double> q = HeartRate.qSDNN();
-	// 	SLastCount<Double> sink = S.lastCount();
+		Query<Integer,Double> q = HeartRate.qSDNN();
+		SLastCount<Double> sink = S.lastCount();
 		
-	// 	Q.execute(Data.ecgStream("100.csv"), q, sink);
+		Q.execute(Data.ecgStream("100.csv"), q, sink);
 
-	// 	assertEquals(1, sink.count);
-	// 	double threshold = sink.last;
-	// 	assert(10.0 <= threshold && threshold <= 200.0);
-	// }
+		assertEquals(1, sink.count);
+		double threshold = sink.last;
+		assert(10.0 <= threshold && threshold <= 200.0);
+	}
 
-	// @Test
-	// public void testRMSSD() {
-	// 	System.out.println("***** Test RMSSD *****");
+	@Test
+	public void testRMSSD() {
+		System.out.println("***** Test RMSSD *****");
 	
-	// 	Query<Integer,Double> q = HeartRate.qRMSSD();
-	// 	SLastCount<Double> sink = S.lastCount();
+		Query<Integer,Double> q = HeartRate.qRMSSD();
+		SLastCount<Double> sink = S.lastCount();
 		
-	// 	Q.execute(Data.ecgStream("100.csv"), q, sink);
+		Q.execute(Data.ecgStream("100.csv"), q, sink);
 
-	// 	assertEquals(1, sink.count);
-	// 	double threshold = sink.last;
-	// 	assert(10.0 <= threshold && threshold <= 200.0);
-	// }
+		assertEquals(1, sink.count);
+		double threshold = sink.last;
+		assert(10.0 <= threshold && threshold <= 200.0);
+	}
 
-	// @Test
-	// public void testPNN50() {
-	// 	System.out.println("***** Test pNN50 *****");
+	@Test
+	public void testPNN50() {
+		System.out.println("***** Test pNN50 *****");
 	
-	// 	Query<Integer,Double> q = HeartRate.qPNN50();
-	// 	SLastCount<Double> sink = S.lastCount();
+		Query<Integer,Double> q = HeartRate.qPNN50();
+		SLastCount<Double> sink = S.lastCount();
 		
-	// 	Q.execute(Data.ecgStream("100.csv"), q, sink);
+		Q.execute(Data.ecgStream("100.csv"), q, sink);
 
-	// 	assertEquals(1, sink.count);
-	// 	double threshold = sink.last;
-	// 	assert(1.0 <= threshold && threshold <= 50.0);
-	// }
+		assertEquals(1, sink.count);
+		double threshold = sink.last;
+		assert(1.0 <= threshold && threshold <= 50.0);
+	}
 
-	// @Test
-	// public void testThreshold() {
-	// 	System.out.println("***** Test Threshold *****");
+	@Test
+	public void testThreshold() {
+		System.out.println("***** Test Threshold *****");
 	
-	// 	Query<Integer,Double> q = TrainModel.qLengthAvg();
-	// 	SLastCount<Double> sink = S.lastCount();
+		Query<Integer,Double> q = TrainModel.qLengthAvg();
+		SLastCount<Double> sink = S.lastCount();
 		
-	// 	Q.execute(Data.ecgStream("100.csv"), q, sink);
+		Q.execute(Data.ecgStream("100.csv"), q, sink);
 
-	// 	assertEquals(1, sink.count);
-	// 	double threshold = sink.last;
-	// 	assert(10 <= threshold && threshold <= 1000);
-	// }
+		assertEquals(1, sink.count);
+		double threshold = sink.last;
+		assert(10 <= threshold && threshold <= 1000);
+	}
 
 	@Test
 	public void testPeakDetection01() {
@@ -127,46 +127,46 @@ public class UTestECG {
 		assertEquals(370, tsPeak2);
 	}
 
-	// @Test
-	// public void testPeakDetection03() {
-	// 	System.out.println("***** Test Peak Detection (3) *****");
+	@Test
+	public void testPeakDetection03() {
+		System.out.println("***** Test Peak Detection (3) *****");
 	
-	// 	Query<Integer,Long> q = PeakDetection.qPeaks();
-	// 	SLastCount<Long> sink = S.lastCount();
+		Query<Integer,Long> q = PeakDetection.qPeaks();
+		SLastCount<Long> sink = S.lastCount();
 		
-	// 	Q.execute(Data.ecgStream("100-samples-800.csv"), q, sink);
+		Q.execute(Data.ecgStream("100-samples-800.csv"), q, sink);
 
-	// 	assertEquals(3, sink.count);
-	// 	long tsPeak3 = sink.last.longValue();
-	// 	assertEquals(663, tsPeak3);
-	// }
+		assertEquals(3, sink.count);
+		long tsPeak3 = sink.last.longValue();
+		assertEquals(663, tsPeak3);
+	}
 
-	// @Test
-	// public void testPeakDetection04() {
-	// 	System.out.println("***** Test Peak Detection (4) *****");
+	@Test
+	public void testPeakDetection04() {
+		System.out.println("***** Test Peak Detection (4) *****");
 	
-	// 	Query<Integer,Long> q = PeakDetection.qPeaks();
-	// 	SLastCount<Long> sink = S.lastCount();
+		Query<Integer,Long> q = PeakDetection.qPeaks();
+		SLastCount<Long> sink = S.lastCount();
 		
-	// 	Q.execute(Data.ecgStream("100-samples-1000.csv"), q, sink);
+		Q.execute(Data.ecgStream("100-samples-1000.csv"), q, sink);
 
-	// 	assertEquals(4, sink.count);
-	// 	long tsPeak3 = sink.last.longValue();
-	// 	assertEquals(947, tsPeak3);
-	// }
+		assertEquals(4, sink.count);
+		long tsPeak3 = sink.last.longValue();
+		assertEquals(947, tsPeak3);
+	}
 
-	// @Test
-	// public void testPeakDetection17() {
-	// 	System.out.println("***** Test Peak Detection (17) *****");
+	@Test
+	public void testPeakDetection17() {
+		System.out.println("***** Test Peak Detection (17) *****");
 	
-	// 	Query<Integer,Long> q = PeakDetection.qPeaks();
-	// 	SLastCount<Long> sink = S.lastCount();
+		Query<Integer,Long> q = PeakDetection.qPeaks();
+		SLastCount<Long> sink = S.lastCount();
 		
-	// 	Q.execute(Data.ecgStream("100.csv"), q, sink);
+		Q.execute(Data.ecgStream("100.csv"), q, sink);
 
-	// 	assertEquals(17, sink.count);
-	// 	long tsPeak3 = sink.last.longValue();
-	// 	assertEquals(4765, tsPeak3);
-	// }
+		assertEquals(17, sink.count);
+		long tsPeak3 = sink.last.longValue();
+		assertEquals(4765, tsPeak3);
+	}
 
 }
